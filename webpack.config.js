c02cbbd9a0742186d8cd1441c34e28021ce652eb
arilotter <arilotter@gmail.com>
2016-12-18 23:12:05 -0500

    changed webpack config formatting to make js style consistent

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-var path = require('path');
- 
-var config = {
+const path = require('path');
+
+const config = {
   context: path.join(__dirname, 'src'),
   entry: [
-    './main.js',
+    './app.js'
   ],
   output: {
     path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['babel'],
-      },
-    ],
+        loaders: ['babel']
+      }
+    ]
   },
   resolveLoader: {
     root: [
-      path.join(__dirname, 'node_modules'),
-    ],
+      path.join(__dirname, 'node_modules')
+    ]
   },
   resolve: {
     root: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+      path.join(__dirname, 'node_modules')
+    ]
+  }
 };
-module.exports = config
+module.exports = config;
