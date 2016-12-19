c3ef1ac62cb6e426d4b6ec30ec43bc10f2dff32c
arilotter <arilotter@gmail.com>
2016-12-18 20:32:59 -0500

    added react

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // ReactDOM.render(
+  //   React.createElement(someElement),
+  //   document.getElementById('mount')
+  // );
+});
