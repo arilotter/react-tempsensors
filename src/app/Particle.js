2dea70c4c03251f76aa6b7e8e577d7556d2a2827
Ari Lotter <arilotter@gmail.com>
2017-12-25 13:56:59 -0500

    hide particle js with old api creds

--- src/app/Particle.js
+++
@@ -1,8 +0,0 @@
-const Particle = {
-  deviceId: '54ff6d066672524826571267',
-  accessToken: '9b4044d39b16c232958a0c8a3a3f9cd5d7809096'
-};
-
-Particle.endpoint = `https://api.particle.io/v1/devices/${Particle.deviceId}`;
-
-export default Particle;
