cb3eeced6c64ec244f6efc7bcb972766b92468b2
Ari Lotter <arilotter@gmail.com>
2017-12-25 15:45:17 -0500

    new heating animation

--- src/app/Thermostat.js
+++ src/app/Thermostat.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import classNames from 'classnames';
-import Button from 'react-toolbox/lib/button';
-import { Card } from 'react-toolbox/lib/card';
-import styles from './Thermostat.scss';
+import React from "react";
+import classNames from "classnames";
+import Button from "react-toolbox/lib/button";
+import { Card } from "react-toolbox/lib/card";
+import styles from "./Thermostat.scss";
 
-const Thermostat = (props) => {
+const Thermostat = props => {
   const isHeating = props.temperature < props.setpoint;
   return (
     <Card className={styles.paper}>
       <h1 className={styles.roomLabel}>{props.label}</h1>
-      <div className={classNames(styles.temperatureIndicator,
-        { [styles.heating]: isHeating }
-      )}>{props.temperature}</div>
+      <div className={styles.temperatureIndicator}>{props.temperature}</div>
       <div className={styles.setpoint}>
         <span>
           {isHeating ? <span>heating to</span> : <span>set to</span>}&nbsp;
@@ -22,18 +20,25 @@ const Thermostat = (props) => {
         <Button
           flat
           onTouchTap={() => props.setSetpoint(props.setpoint - 0.5)}
-          label='Colder'
-          icon='remove'
+          label="Colder"
+          icon="remove"
           className={styles.colderButton}
         />
         <Button
           flat
           onTouchTap={() => props.setSetpoint(props.setpoint + 0.5)}
-          label='Hotter'
-          icon='add'
+          label="Hotter"
+          icon="add"
           className={styles.hotterButton}
         />
       </div>
+      {isHeating ? (
+        <div className={styles.waveContainer}>
+          <div className={styles.wave} />
+          <div className={styles.wave} />
+          <div className={styles.wave} />
+        </div>
+      ) : null}
     </Card>
   );
 };
