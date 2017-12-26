666d7a09e392455ba4eead46d553c95786812e62
Ari Lotter <arilotter@gmail.com>
2017-12-25 20:55:07 -0500

    top bar red when on hold, remove refresh button

--- src/app/Chrome.js
+++ src/app/Chrome.js
@@ -3,18 +3,23 @@ import AppBar from "react-toolbox/lib/app_bar";
 import Link from "react-toolbox/lib/link";
 import Navigation from "react-toolbox/lib/navigation";
 import styles from "./Chrome.scss";
+import holdStyles from "./ChromeHold.scss";
+import classNames from "classnames";
 
 const Chrome = props => (
-  <AppBar title="Temperature Control" theme={styles}>
+  <AppBar
+    fixed
+    title="Temperature Control"
+    theme={props.hold ? holdStyles : styles}
+  >
     <Navigation type="horizontal">
       <Link
         label="Hold"
         icon={props.hold ? "pause_circle_filled" : "pause_circle_outline"}
-        onClick={props.doHold}
-        className={props.hold ? styles.holdButton : ''}
+        onTouchTap={props.doHold}
+        className={props.hold ? styles.holdButton : ""}
         active={!!props.hold}
       />
-      <Link label="Refresh" icon="refresh" onClick={props.doRefresh} />
     </Navigation>
   </AppBar>
 );
